2b9b53bf8b31a1cd1952b79026b5c685352ef8db
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-03-04 15:50:13 +0700

    Add server and proxy

---
+++ cmd/proxy/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"grpc-gateway-funnzy-dev/pbs"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/grpclog"
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Register gRPC server endpoint
+	// Note: Make sure the gRPC server is running properly and accessible
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := pbs.RegisterHelloHandlerFromEndpoint(ctx, mux, "localhost:9000", opts)
+	if err != nil {
+		return err
+	}
+
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	return http.ListenAndServe(":8081", mux)
+}
+
+func main() {
+	flag.Parse()
+
+	if err := run(); err != nil {
+		grpclog.Fatal(err)
+	}
+}
